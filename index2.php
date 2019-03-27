944a3d977d1e68e4cd8f9bdb630a39374d1ed125
DmytroPol <33494689+DmytroPol@users.noreply.github.com>
2019-03-27 20:47:10 +0200

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,21 +1,11 @@
 <?php
 /**
- * Предистория:
- * Хотел сделать качество по максимуму с учетом дедлайна, время работы над заданием:21,54 - 00,48 (3 часа)
- * ==================================================
- * Весь бэк построил так:
- * 1) Данных приходят на сервер через AJAX запрос
- * 2) Скрипт обрабатывает его, и мы получаем нужное пользователю действие
- * 3) Используя функцию doTable мы создаем таблицу, и возвращаем её код
- * 4) стандартный echo ответ
+ * data comes to the server after the request
+ * the script is processing it
+ * doTable //for create table and return code
+ * echo //response
  *
  * ==================================================
- */
-/**
- * @param $var
- * ======================================
- * Ф-я для отладки кода
- * ======================================
  */
 function d($var)
 {
@@ -23,24 +13,21 @@ function d($var)
     print_r($var);
     die;
 }
-/**
- *====================================================
- * Ф-я для построения таблицы
- * ====================================================
- * @param array $columnName ==> ручками пишем нужное название столбцов таблицы
- * @param array $dataArray ==> данные для столбцов (будут доставатся с SQL запроса)
+/*
+ * @param array $columnName //table column name
+ * @param array $dataArray //data for columns
  * @return string
  */
 function doTable(array $columnName,array $dataArray)
 {
     $table="<table class='table table-hover table_response' border='2'><thead><tr>";
-    // шапка для результирующей таблицы
+    // header for the result table
     foreach ($columnName as $th)
       {
           $table.="<th>{$th}</th>";
       }
       $table.="</tr></thead><tbody>";
-    // тело таблицы
+    // body of the table
     foreach ($dataArray as $items )
     {
         $table.="<tr>";
@@ -52,18 +39,13 @@ function doTable(array $columnName,array $dataArray)
     }
     return $table.=" </tbody></table>";
 }
-//1) подключаем БД + после у меня полезли ????, пришлось на месте прибить кодировку
+// connect the database
 $db=new mysqli('localhost','root','','yii2basic');
 $db->set_charset('utf8');
-// 2) готовим сам запрос и переменную $result для ответа
+// query and $ result variable for response
 $request=$_POST['type'];
 $result='';
-// Перечень кейсов:
-// 1) Вывести таблицу с товарами
-// 2) Вывести № заказа, название товара
-// 3) Номер заказа, имя товара, цена, количество, имя оператора который
-// за которым числится заказ ГДЕ количество товара >2 И id оператора 10 ИЛИ 12
-// 4) Имя товара, количество товара, и сумма (price) по каждому товару (сгруппировать)
+
 switch ($request)
 {
     case 1:
@@ -71,7 +53,6 @@ switch ($request)
         $result=doTable(['ID товара','Имя товара'],$products);
         break;
     case 2:
-        // ID заказа с инпута к заданию
         $orderId=$_POST['order'];
         $order=$db->query('SELECT requests.id, offers.name
         FROM requests
@@ -88,15 +69,15 @@ switch ($request)
         LEFT JOIN operators
         ON requests.operator_id=operators.id
         WHERE requests.count>2 AND (requests.operator_id=10 OR requests.operator_id=12);")->fetch_all(MYSQLI_ASSOC);
-        $result=doTable(['№ заказа','Имя товара','Цена товара','Стоимость товара','ФИО менеджера'],$orderList);
+        $result=doTable(['№ order','Name of product','The price of the product','Cost of goods','Surname of the manager'],$orderList);
         break;
     case 4:
         $itemOffers=$db->query("
-        SELECT  offers.name AS товар, SUM(requests.price*requests.count) AS 'общая сумма по товару'
+        SELECT  offers.name AS product, SUM(requests.price*requests.count) AS 'the total amount of goods'
         FROM requests
         LEFT JOIN offers
         ON requests.offer_id=offers.id
         GROUP BY offers.name;")->fetch_all(MYSQLI_ASSOC);
-        $result=doTable(['Наименование товара','Общая сумма по товару'],$itemOffers);
+        $result=doTable(['Name of product','The total amount of goods'],$itemOffers);
 }
 echo $result;
